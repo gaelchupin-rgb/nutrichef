5757b82572c4ad62f5e52a80642b71eb3e86b62c
Kaspo <gael.chupin@outlook.com>
2025-08-27 10:21:51 +0200

    Add dotenv configuration and env templates

--- Nutrishop/nutrishop-v2/next.config.mjs
+++ Nutrishop/nutrishop-v2/next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import dotenv from 'dotenv';
+dotenv.config();
 
-export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
